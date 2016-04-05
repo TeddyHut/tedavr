73483675c79a430cf3242a3d3051b171c91d9bfc
Teddy.Hutteddy <teddy.hutteddy@gmail.com>
2016-04-05 18:33:59 +1000

    updated set [button] to v2.0

--- source/button.c
+++ source/button.c
@@ -1,112 +1,85 @@
-#include "../include/button.h"
+#include "../include/tedavr/button.h"
 
-uint8_t _buttonBV(uint8_t bit) {
-	return(1<<bit);
+void button_clear(Button *const button) {
+	button->flag_function_updateCurrent = 0;
+	button->flag_function_updatePrevious = 0;
+	button->flag_function_updatePushed = 0;
+	button->flag_function_updateReleased = 0;
+	button->flag_logic_idle = 0;
+	button->flag_state_current = 0;
+	button->flag_state_previous = 0;
+	button->flag_state_pushed = 0;
+	button->flag_state_released = 0;
+	button->data_shift_portBit = 0;
+	button->data_port_p = NULL;
 }
 
-void buttonSet_attrib_auto(Button* button,uint8_t state) {
-	if(state) button->attrib|=_buttonBV(BUTTON_ATTRIB_AUTO);
-	else button->attrib&=~(_buttonBV(BUTTON_ATTRIB_AUTO));
+void button_defaultSetup(Button *const button) {
+	button->flag_function_updateCurrent = 1;
+	button->flag_function_updatePrevious = 1;
+	button->flag_function_updatePushed = 1;
+	button->flag_function_updateReleased = 1;
+	button->flag_logic_idle = 1;
+	button->flag_state_current = 0;
+	button->flag_state_previous = 0;
+	button->flag_state_pushed = 0;
+	button->flag_state_released = 0;
+	button->data_shift_portBit = 0;
+	button->data_port_p = NULL;
 }
 
-void buttonSet_attrib_currentState(Button* button,uint8_t state) {
-	if(state) button->attrib|=_buttonBV(BUTTON_ATTRIB_CURRENT_STATE);
-	else button->attrib&=~(_buttonBV(BUTTON_ATTRIB_CURRENT_STATE));
+void button_update(Button *const button) {
+	if (button->flag_function_updatePrevious)
+		button->flag_state_previous = button->flag_state_current;
+	if (button->flag_function_updateCurrent)
+		if(button->data_port_p != NULL)
+			button->flag_state_current = (*(button->data_port_p) & _BV(button->data_shift_portBit)) ? (1 - (button->flag_logic_idle)) : (0 + (button->flag_logic_idle));	//Probs a better way to do...
+	if (button->flag_function_updatePushed)
+		button->flag_state_pushed = (button->flag_state_current && (!(button->flag_state_previous))) ? 1 : 0;
+	if (button->flag_function_updateReleased)
+		button->flag_state_released = ((!(button->flag_state_current)) && button->flag_state_previous) ? 1 : 0;
 }
 
-void buttonSet_attrib_previousState(Button* button,uint8_t state) {
-	if(state) button->attrib|=_buttonBV(BUTTON_ATTRIB_PREVIOUS_STATE);
-	else button->attrib&=~(_buttonBV(BUTTON_ATTRIB_PREVIOUS_STATE));
+#endif
+
+
+uint8_t buttonGet_attrib_released(Button const*const button) {
+	return(general_GetBitFlag_u8(&(button->attrib), BUTTON_ATTRIB_RELEASED));
 }
 
-void buttonSet_attrib_pushed(Button* button,uint8_t state) {
-	if(state) button->attrib|=_buttonBV(BUTTON_ATTRIB_PUSHED);
-	else button->attrib&=~(_buttonBV(BUTTON_ATTRIB_PUSHED));
+uint8_t buttonGet_attrib_portBit(Button const*const button) {
+	return((button->attrib & BUTTON_PORT_BIT_MASK)>>BUTTON_PORT_BIT_SHIFT);
 }
 
-void buttonSet_attrib_released(Button* button,uint8_t state) {
-	if(state) button->attrib|=_buttonBV(BUTTON_ATTRIB_RELEASED);
-	else button->attrib&=~(_buttonBV(BUTTON_ATTRIB_RELEASED));
-}
-
-void buttonSet_attrib_portBit(Button* button,uint8_t state) {
-	button->attrib&=~((uint8_t)BUTTON_PORT_BIT_MASK);
-	button->attrib|=(state<<BUTTON_PORT_BIT_SHIFT);
-}
-
-void buttonSet_port(Button* button,volatile uint8_t* state) {
-	button->port = state;
-}
-
-uint8_t buttonGet_attrib_auto(Button* button) {
-	return((button->attrib&_buttonBV(BUTTON_ATTRIB_AUTO))>>BUTTON_ATTRIB_AUTO);
-}
-
-uint8_t buttonGet_attrib_currentState(Button* button) {
-	return((button->attrib&_buttonBV(BUTTON_ATTRIB_CURRENT_STATE))>>BUTTON_ATTRIB_CURRENT_STATE);
-}
-
-uint8_t buttonGet_attrib_previousState(Button* button) {
-	return((button->attrib&_buttonBV(BUTTON_ATTRIB_PREVIOUS_STATE))>>BUTTON_ATTRIB_PREVIOUS_STATE);
-}
-
-uint8_t buttonGet_attrib_pushed(Button* button) {
-	return((button->attrib&_buttonBV(BUTTON_ATTRIB_PUSHED))>>BUTTON_ATTRIB_PUSHED);
-}
-
-uint8_t buttonGet_attrib_released(Button* button) {
-	return((button->attrib&_buttonBV(BUTTON_ATTRIB_RELEASED))>>BUTTON_ATTRIB_RELEASED);
-}
-
-uint8_t buttonGet_attrib_portBit(Button* button) {
-	return((button->attrib&BUTTON_PORT_BIT_MASK)>>BUTTON_PORT_BIT_SHIFT);
-}
-
-volatile uint8_t* buttonGet_port(Button* button) {
+volatile uint8_t* buttonGet_port(Button const*const button) {
 	return(button->port);
 }
 
-void button_clear(Button* button) {
+void button_clear(Button*const button) {
 	button->attrib = 0;
 }
 
-void button_setDefault(Button* button) {
+void button_setDefault(Button*const button) {
 	button_clear(button);
-	buttonSet_attrib_auto(button,1);
+	buttonSet_attrib_auto(button, 1);
 }
 
-void button_runover(Button* button) {
+void button_runover(Button*const button) {
 	button_runoverSegment0(button);
 	button_runoverSegment1(button);
 	button_runoverSegment2(button);
-	button_runoverSegment3(button);
 }
 
-void button_runoverSegment0(Button* button) {
-	buttonSet_attrib_pushed(button,0);
-	buttonSet_attrib_released(button,0);
+void button_runoverSegment0(Button*const button) {
+	if(buttonGet_attrib_auto(button))
+		buttonSet_attrib_currentState(button, bit_is_set(*(button->port), buttonGet_attrib_portBit(button)));
 }
 
-void button_runoverSegment1(Button* button) {
-	if(buttonGet_attrib_auto(button)) {
-		if(*button->port&_buttonBV(buttonGet_attrib_portBit(button))) {
-			buttonSet_attrib_currentState(button,1);
-		}
-		else {
-			buttonSet_attrib_currentState(button,0);
-		}
-	}
+void button_runoverSegment1(Button*const button) {
+	buttonSet_attrib_pushed(button, ((!buttonGet_attrib_previousState(button)) && buttonGet_attrib_currentState(button)));
+	buttonSet_attrib_released(button, (buttonGet_attrib_previousState(button) && (!buttonGet_attrib_currentState(button))));
 }
 
-void button_runoverSegment2(Button* button) {
-	if((buttonGet_attrib_previousState(button) == 0)&&(buttonGet_attrib_currentState(button) == 1)) {
-		buttonSet_attrib_pushed(button,1);
-	}
-	if((buttonGet_attrib_previousState(button) == 1)&&(buttonGet_attrib_currentState(button) == 0)) {
-		buttonSet_attrib_released(button,1);
-	}
-}
-
-void button_runoverSegment3(Button* button) {
-	buttonSet_attrib_previousState(button,buttonGet_attrib_currentState(button));
+void button_runoverSegment2(Button*const button) {
+	buttonSet_attrib_previousState(button, buttonGet_attrib_currentState(button));
 }
