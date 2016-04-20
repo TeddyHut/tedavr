cb9855e43768260503a2cf2f1bdf263fa90aca2a
Teddy.Hutteddy <teddy.hutteddy@gmail.com>
2016-04-20 15:17:03 +1000

    added set[general]

---
+++ include/tedavr/general.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <avr/sfr_defs.h>
+#include <inttypes.h>
+
+void general_SetBitFlag_u8(volatile uint8_t*const byte, uint8_t const bit_significance, uint8_t const state);	//Could rename that to bit_shift
+uint8_t general_GetBitFlag_u8(volatile uint8_t const*const byte, uint8_t const bit_significance);				//Someone tell my why I'm making it a pointer.
---
+++ source/general.c
@@ -0,0 +1,9 @@
+#include "../include/tedavr/general.h"
+
+void general_SetBitFlag_u8(volatile uint8_t*const byte, uint8_t const bit_significance, uint8_t const state) {
+	*byte = ((*byte & _BV(bit_significance)) | ((state ? 1 : 0) << bit_significance));
+}
+
+uint8_t general_GetBitFlag_u8(uint8_t const*const byte, uint8_t const bit_significance) {
+	return(((*byte) & _BV(bit_significance)) ? 1 : 0);
+}
