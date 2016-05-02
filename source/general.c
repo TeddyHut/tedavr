b5b4024c74c6c73488b66a0282d8f046c3527127
Teddy.Hutteddy <teddy.hutteddy@gmail.com>
2016-05-03 07:01:15 +1000

    fixed bug in [general.c]

--- source/general.c
+++ source/general.c
@@ -1,7 +1,7 @@
 #include "../include/tedavr/general.h"
 
 void general_SetBitFlag_u8(volatile uint8_t*const byte, uint8_t const bit_significance, uint8_t const state) {
-	*byte = ((*byte & _BV(bit_significance)) | ((state ? 1 : 0) << bit_significance));
+	*byte = ((*byte & ~_BV(bit_significance)) | ((state ? 1 : 0) << bit_significance));
 }
 
 uint8_t general_GetBitFlag_u8(volatile uint8_t const*const byte, uint8_t const bit_significance) {
